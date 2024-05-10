a49a96761ad20311b3bfc13905a6e1e0b4daeac5
Aidar Ibraev <aidaribraev32@gmail.com>
2024-05-10 17:29:29 +0600

    Commit 1.3.16. Props, PropsType. The most important topic.

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -12,10 +12,8 @@ export const MyPosts = () => {
                 <button>Remove post</button>
             </div>
             <div className={s.posts}>
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                <Post message={"Hi, how are you??"} likesCount={18} />
+                <Post message={"It's my first post"} likesCount={22} />
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import s from "./Post.module.css";
 
-export const Post = () => {
+type PostPropsType = {
+    message: string
+    likesCount: number
+}
+
+export const Post = ({message, likesCount}: PostPropsType) => {
+
+
     return (
         <div className={s.item}>
             <img src="https://assets.nick.com/uri/mgid:arc:imageassetref:shared.nick.us:9ca6d0d8-a621-4614-ad00-ccebe3084a62?quality=0.7&gen=ntrn&legacyStatusCode=true" alt="avatar"/>
-            post 1
-            <div>like</div>
+            {message}
+            <div>like {likesCount}</div>
         </div>
     );
 };
